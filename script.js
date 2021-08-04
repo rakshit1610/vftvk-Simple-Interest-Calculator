9c1de490ea3ce42df17aa9a67ea4b82c4a13b779
rakshit1610 <rakshitpanjwani@gmail.com>
2021-08-04 19:19:51 +0530

    fixed the design

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    si= (Number(p)*Number(r)*Number(t))/100
+    end= Number(t)+2021;
     
+    document.getElementById('res').innerHTML=`If you deposit ${p}<br/> at an interest rate of ${r} %<br/>You will receive an amount of ${si}<br/> in the year ${end}`
 }
         
+function change()
+{
+    r = document.getElementById("rate").value;
+
+    document.getElementById('percentage').innerHTML=`${r} %`;
+}
